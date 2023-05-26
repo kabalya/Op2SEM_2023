479ad5431253eff16637636bbae6162c739ae858
kabalya <112944418+kabalya@users.noreply.github.com>
2023-05-26 23:16:43 +0300

    Update Main.cpp

--- 2 часть/Main.cpp
+++ 2 часть/Main.cpp
@@ -1,26 +1,28 @@
 ﻿#include<iostream>
-#include<algorithm>
-#include <array>
-#include "SubSet.h"
-#include "Massive.cpp"
+
+#include "Subset.h"
+
 using namespace Subset;
 
-int main() 
+int main()
 {
 
-    setlocale(LC_ALL, "Russian");
+   setlocale(LC_ALL, "Russian");
 
-    SubSet First({ 5, 3, 4 });
+    SubSet First({ 1, 2, 3,5 });
 
-    SubSet Second({ 9, 0, 5, 7, 4, 3 });
+    SubSet Second({ 6, 2, 4, 6, 8, 9 });
 
     First.operator+(Second)->printSubset();
-    
-    Second.operator+=(6)->printSubset();
-    First.operator==(Second);
-    cout <<"\n";
-    Second.operator-=(6)->operator-=(7)->printSubset();
-    
-   
 
+    Second.operator+=(6)->printSubset();
+    cout << "\n";
+    First.operator==(Second);
+    cout << "\n";
+    First.operator!=(Second);
+
+    Second.operator-=(6)->operator-=(7)->printSubset();
+
+
+    return 0;
 }
