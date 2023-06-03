c483f1a50a1f9c96edbf7ba2531d476f5bba7b0a
kabalya <112944418+kabalya@users.noreply.github.com>
2023-06-03 18:15:04 +0300

    LAB_OP_6

---
+++ find_backwords.h
@@ -0,0 +1,35 @@
+#pragma once
+#ifndef LAB6_FIND_BACKWORDS_H
+#define LAB6_FIND_BACKWORDS_H
+#include <iterator>
+#include <algorithm>
+#include "iostream"
+namespace Functions {
+    template<typename Iterator, typename T>
+    Iterator find_backwords(Iterator first, Iterator last, const T& value)
+    {
+        Iterator result = last;
+        
+
+        for (Iterator it = first; it != last; ++it)
+        {
+            if (*it == value) 
+            {
+                result = it;
+              
+            }
+        }
+        
+        if (result != last) 
+        {
+            std::cout << "Position FIND_BACKWORDS : " << std::distance(first, result) << std::endl;
+        }
+        else
+        {
+            std::cout << "Element not found!" << std::endl;
+        }
+        return result;
+    }
+}
+
+#endif
